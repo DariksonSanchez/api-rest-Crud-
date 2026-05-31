075766574c7a8e31c699aa88e57f786bc2a7f424
Darikson Sanchez <sanchezdarikson6@gmail.com>
2026-05-31 13:50:28 -0400

    first commit

--- obj/Release/net8.0/TodoListapi.AssemblyInfo.cs
+++ obj/Release/net8.0/TodoListapi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoListapi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d8729ebf7969f71cdc8ed2528e7b0c95a8b7d78")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoListapi")]
